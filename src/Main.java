37f29fd78463825ad0c30c3cdff0c3f18cd1e870
Ramil <rdgalimov@yandex.ru>
2022-11-11 10:44:57 +0300

    Домашнее задание переменные

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
-        var dog = 8.0;
+        double dog = 8;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
